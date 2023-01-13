6486e34e8f0cb122aa95a13f13d3ed33b861c910
nitish <diluboruah@gmail.com>
2023-01-13 13:35:36 +0530

    4th sol

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,6 +3,17 @@ package com.driver;
 import java.util.Scanner;
 
 public class Main {
+    public static class Product{
+        public int product(int x, int y) {
+            return x*y;
+        }
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+        public double product(double x, double y) {
+            return x*y;
+        }
+    }
     public static void main(String []args){
         Scanner sc=new Scanner(System.in);
         Product p=new Product();
@@ -12,16 +23,5 @@ public class Main {
         System.out.println(p.product(sc.nextDouble(),sc.nextDouble()));
 
 
-    }
-}
-class Product{
-    public int product(int x, int y) {
-        return x*y;
-    }
-    public int product(int x, int y, int z) {
-        return x*y*z;
-    }
-    public double product(double x, double y) {
-        return x*y;
     }
 }
