885ca3c889819a69be2a3d8c4d100d15785e51c0
nitish <diluboruah@gmail.com>
2023-01-13 12:53:49 +0530

    2nd sol

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,11 +1,18 @@
 package com.driver;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int z=sc.nextInt();
         Product p=new Product();
-        System.out.println(p.product(2,3));
-        System.out.println(p.product(2,3,4));
-        System.out.println(p.product(2.0,3.0));
+
+        System.out.println(p.product(x,y));
+        System.out.println(p.product(x,y,z));
+        System.out.println(p.product(Double.valueOf(x),Double.valueOf(y)));
 
 
     }
